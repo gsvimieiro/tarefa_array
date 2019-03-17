1e8fc73ae0e673704cf386d8a53227f7343236eb
gsvimieiro01 <gsvimieiro@gmail.com>
2019-03-17 16:02:45 -0300

    Primeiro commit

---
+++ src/test/java/com/cast/tarefaarray/TarefaArrayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cast.tarefaarray;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TarefaArrayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
